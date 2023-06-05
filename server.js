024203f32cdcb300569127ea2d27029972290f6f
Justin Canzonetta <canzonej@oregonstate.edu>
2023-06-04 21:05:35 -0700

    Enable trust proxy for Express for gcloud deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes/index.routes.js';
 const app = express();
 
+app.enable('trust proxy');
 app.use('/', routes);
 
 // Listen to the App Engine-specified port, or 8080 otherwise
